ac9c20b5b3810b05e36188372ebf067492480ea8
gauravk-io <gauravkr7265@gmail.com>
2025-07-11 22:20:39 +0530

    docs: add comments to VideoUploadForm placeholder component

--- app/components/VideoUploadForm.tsx
+++ app/components/VideoUploadForm.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
+// Placeholder component for the video upload form.
+// Will later include input fields, file upload logic, etc.
 function VideoUploadForm() {
-  return <div>assignment</div>;
+  return <div>assignment</div>; // Temporary placeholder content
 }
 
 export default VideoUploadForm;
